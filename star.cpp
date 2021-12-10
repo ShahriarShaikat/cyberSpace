fbf88fe4a05fce4ea5b417a4b897051f9c172edd
Shovon <shovon.zzz5@gmail.com>
2021-12-10 18:52:01 +0600

    star color added

--- star.cpp
+++ star.cpp
@@ -4,12 +4,13 @@ void displayStar() {
 
     glTranslatef(0.5f, 0.6f, 0.0f);
     glBegin (GL_POLYGON);
-        glColor3f(0.97, 1, 0.63);
+        glColor3f(0.92, 0.65, 0.49);
         glVertex2f(0.0f,0.2f);
         glVertex2f(0.1f,0.1f);
         glVertex2f(0.2f,0.05f);
         glVertex2f(0.1f,0.0f);
         glVertex2f(0.2f,-0.1f);
+        glColor3f(0.99, 1, 0.94);
         glVertex2f(0.0f,0.0f);
         glVertex2f(-0.2f,-0.1f);
         glVertex2f(-0.1f,0.0f);
@@ -21,11 +22,13 @@ void displayStar() {
 
     glTranslatef(0.0f, 0.8f, 0.0f);
     glBegin (GL_POLYGON);
+         glColor3f(0.45, 0.58, 0.78);
         glVertex2f(0.0f,0.2f);
         glVertex2f(0.1f,0.1f);
         glVertex2f(0.2f,0.05f);
         glVertex2f(0.1f,0.0f);
         glVertex2f(0.2f,-0.1f);
+        glColor3f(0.91, 0.97, 1);
         glVertex2f(0.0f,0.0f);
         glVertex2f(-0.2f,-0.1f);
         glVertex2f(-0.1f,0.0f);
@@ -37,11 +40,13 @@ void displayStar() {
 
     glTranslatef(0.5f, 0.1f, 0.0f);
     glBegin (GL_POLYGON);
+         glColor3f(0.45, 0.58, 0.78);
         glVertex2f(0.0f,0.2f);
         glVertex2f(0.1f,0.1f);
         glVertex2f(0.2f,0.05f);
         glVertex2f(0.1f,0.0f);
         glVertex2f(0.2f,-0.1f);
+        glColor3f(0.91, 0.97, 1);
         glVertex2f(0.0f,0.0f);
         glVertex2f(-0.2f,-0.1f);
         glVertex2f(-0.1f,0.0f);
@@ -53,11 +58,13 @@ void displayStar() {
 
     glTranslatef(-0.6f, 0.6f, 0.0f);
     glBegin(GL_POLYGON);
+         glColor3f(0.92, 0.65, 0.49);
         glVertex2f(0.0f,0.2f);
         glVertex2f(0.1f,0.1f);
         glVertex2f(0.2f,0.05f);
         glVertex2f(0.1f,0.0f);
         glVertex2f(0.2f,-0.1f);
+        glColor3f(0.99, 1, 0.94);
         glVertex2f(0.0f,0.0f);
         glVertex2f(-0.2f,-0.1f);
         glVertex2f(-0.1f,0.0f);
@@ -69,11 +76,13 @@ void displayStar() {
 
     glTranslatef(-0.5f, 0.1f, 0.0f);
     glBegin(GL_POLYGON);
+         glColor3f(0.45, 0.58, 0.78);
         glVertex2f(0.0f,0.2f);
         glVertex2f(0.1f,0.1f);
         glVertex2f(0.2f,0.05f);
         glVertex2f(0.1f,0.0f);
         glVertex2f(0.2f,-0.1f);
+        glColor3f(0.91, 0.97, 1);
         glVertex2f(0.0f,0.0f);
         glVertex2f(-0.2f,-0.1f);
         glVertex2f(-0.1f,0.0f);
